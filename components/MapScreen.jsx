5fbc6b5596688f3b6c81bddfbc4d87c34cfbf635
Teshell <fayrongeek@gmail.com>
2022-06-19 19:57:53 +0100

    implementing navigation

---
+++ components/MapScreen.jsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+const MapScreen = () => {
+  return (
+    <SafeAreaView>
+      <Text>Here is the Map stuff...</Text>
+    </SafeAreaView>
+  );
+};
+
+export default MapScreen;
+
+const styles = StyleSheet.create({});
